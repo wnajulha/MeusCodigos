04dafaef0484b4e0eef9c0094dc04953393b29a6
Ana Julia <129068318+wnajulha@users.noreply.github.com>
2023-04-13 15:24:55 -0300

    Add files via upload
    primeiro codigo javascript

---
+++ main.js
@@ -0,0 +1,32 @@
+function tocaSom(idElementAudio){
+document.querySelector(idElementAudio).play();
+}
+
+
+const listaDeTeclas =document.querySelectorAll('.tecla')
+
+let contador = 0;
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla .classList[1];
+    const idAudio = `#som_${instrumento}`; //templatestring
+
+    tecla.onclick = function () {
+      tocaSom(idAudio)
+    }
+
+    tecla.onkeydown =function(evento){
+
+      console.log(evento.code)
+      if (evento.code === 'Space' || evento.code === 'Enter'){
+        tecla.classList.add('ativa')
+      }
+      
+    }
+
+    tecla.onkeyup = function(){
+      tecla.classList.remove('ativa')
+    }
+}
